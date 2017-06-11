09db77c017fe39cbeb08c5b7fa2d495924799876
Vincent Delaporte <delaporte.vincent.vd@gmail.com>
2017-06-11 11:29:24 +0200

    Ajout commun.component.ts et html

--- TrelloDriveChat/app/app.module.ts
+++ TrelloDriveChat/app/app.module.ts
@@ -1,38 +1,42 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import { HttpModule, JsonpModule }  from '@angular/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { SocketService } from './socket.service';
-import {AppComponent} from './app.component';
-import {routing} from './app.routing';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 
-import {Drive} from './Drive/drive.component';
-import {Home} from './Home/home.component';
-import {Messagerie} from './Messagerie/messagerie.component';
-import {Projet} from './Projet/projet.component';
-import {Trello} from './Trello/trello.component';
+import { Drive } from './Drive/drive.component';
+import { Home } from './Home/home.component';
+import { Messagerie } from './Messagerie/messagerie.component';
+import { Projet } from './Projet/projet.component';
+import { Trello } from './Trello/trello.component';
+
+import { Commun } from './Commun/commun.component';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        JsonpModule,
-        HttpModule,
-        routing        
-    ],
-    declarations: [
-      AppComponent,
-      Drive,
-      Home,
-      Messagerie,
-      Projet,
-      Trello
-    ],
-    bootstrap: [
-      AppComponent
-    ],
-    providers: [SocketService],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    JsonpModule,
+    HttpModule,
+    routing
+  ],
+  declarations: [
+    Commun,
+    AppComponent,
+    Drive,
+    Home,
+    Messagerie,
+    Projet,
+    Trello
+  ],
+  bootstrap: [
+    AppComponent,
+    Commun
+  ],
+  providers: [SocketService],
 })
 export class AppModule {
 }
