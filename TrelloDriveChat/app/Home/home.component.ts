e446a41aa12fdb7cb13b559cfd49df17784c3d64
ClaireFilatre <claire.filatre35@gmail.com>
2017-06-12 18:58:49 +0200

    css du commun Veuillez saisir le message de validation pour vos modifications. Les lignes

--- TrelloDriveChat/app/Home/home.component.ts
+++ TrelloDriveChat/app/Home/home.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'home',
-    templateUrl: './app/Home/home.html'
+    templateUrl: './app/Home/home.html',
+    styleUrls:['./app/Home/home.css']
 })
 
 export class Home {
